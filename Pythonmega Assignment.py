e82d205e3acc00064ffe948d64b43980e7f6e69b
dhuriyapawan <pawandhuirya.outlook.com>
2023-07-02 13:15:39 +0530

    All Problem Completed

--- Pythonmega Assignment.py
+++ Pythonmega Assignment.py
@@ -11,7 +11,7 @@
 # x=txt.index("t")
 # print(x)
 
-# Q28. Write a code to get the desired output of the following
+# Q28. Write a code to get the desired output of the followingP
 # ```
 
       #string = "Big Data iNeuron"
@@ -199,33 +199,58 @@
 
 
 # Q62. What is the use of get() function?
-
+# The get() method returns the value of the item with the specified key.
+# e.g dictionary.get(keyname, value)
 
 # Q63. What is the use of items() function?
+# The items() method returns a view object. The view object contains the key-value pairs of the dictionary, as tuples in a list.
+# dictionary.items()
 
 # Q64. What is the use of pop() function?
+# The pop() method removes the element at the specified position.
+# list.pop(pos)
 
 # Q65. What is the use of popitems() function?
+# The popitem() method removes the item that was last inserted into the dictionary.
 
 # Q66. What is the use of keys() function?
+# The keys() method returns a view object. The view object contains the keys of the dictionary, as a list.
+# dictionary.keys()
 
 # Q67. What is the use of values() function?
+# Python dictionary values() function is used to return a new view object that contains a list of all the values associated with the keys in the dictionary
+
 
 # Q68. What are loops in Python?
+# A for loop is used for iterating over a sequence.
 
 # Q69. How many type of loop are there in Python?
+# 1)for Loop
+# 2)while loop
+
 
 # Q70. What is the difference between for and while loops?
+# for loop is used to repetetive task again and agin perfroming untill loop value ended.
+# for while loop the veriable value should be true or false according to it loop program excuted.
 
 # Q71. What is the use of continue statement?
+# continue:this statemnet is used to make loop or program running contiously.
 
 # Q72. What is the use of break statement?
+# break: is used to break the looping if we want to  break the progrma and exits.
 
 # Q73. What is the use of pass statement?
+# The pass statement is used as a placeholder for future code
+# A pass statement signals to a loop that there is “no code to execute here.”
 
 # Q74. What is the use of range() function?
+# The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and stops before a specified number.
+# range(start, stop, step)
 
 # Q75. How can you loop over a dictionary?
+# You can loop through a dictionary by using a for loop.
+# When looping through a dictionary, the return value are the keys of the dictionary, but there are methods to return the values as well.
+
 
 
 # ### Coding problems
@@ -364,48 +389,100 @@
 # check_substring(s2, s1)
 
 # Q89. Write a Python program to find words which are greater than given length k.
-def string_check(string_length,my_string):
-	result_string=[]
-	words=my_string.split(" ")
-	for x  in words:
-		if len(x)>string_length:
-			result_string.append(x)
-	return result_string
-string_length=3
-my_string="Welcome to iNeuron Big Data Bootcamp "
-print(f"The string is :",my_string)
-print(string_check(string_length,my_string))
+# def string_check(string_length,my_string):
+# 	result_string=[]
+# 	words=my_string.split(" ")
+# 	for x  in words:
+# 		if len(x)>string_length:
+# 			result_string.append(x)
+# 	return result_string
+# string_length=3
+# my_string="Welcome to iNeuron Big Data Bootcamp "
+# print(f"The string is :",my_string)
+# print(string_check(string_length,my_string))
 
 # Q90. Write a Python program to extract unquire dictionary values.
+# dict1 = {'A' : [1, 3, 5, 4],
+#              'B' : [4, 6, 8, 10],
+#              'C' : [6, 12, 4 ,8],
+#              'D' : [5, 7, 2]}
+
+# print("The original dictionary is : " ,dict1)
+  
+# # Using list comprehension, values() and sorted()
+# res = list(sorted({ele for val in dict1.values() for ele in val}))
+  
+# # print result 
+# print("The unique values list is : " , res) 
 
 # Q91. Write a Python program to merge two dictionary.
+# def Merge(dict1, dict2):
+#     res = {**dict1, **dict2}
+#     return res
+# dict1 = {'x': 10, 'y': 8}
+# dict2 = {'a': 6, 'b': 4}
+# dict3 = Merge (dict1, dict2)
+# print(dict3)
 
 # Q92. Write a Python program to convert a list of tuples into dictionary.
 # ```
-# Input : [('Sachin', 10), ('MSD', 7), ('Kohli', 18), ('Rohit', 45)]
-# Output : {'Sachin': 10, 'MSD': 7, 'Kohli': 18, 'Rohit': 45}
+def convert(tup,di):
+    di=dict(Input)
+    return di
+Input = [('Sachin', 10), ('MSD', 7), ('Kohli', 18), ('Rohit', 45)]
+dictionary={}
+print(convert(Input,dictionary))
+# Output = {'Sachin': 10, 'MSD': 7, 'Kohli': 18, 'Rohit': 45}
 # ```
 
 # Q93. Write a Python program to create a list of tuples from given list having number and its cube in each tuple.
 # ```
-# Input: list = [9, 5, 6]
+# Input: 
+list = [9, 5, 6]
+res=[(val,pow(val,3))for val in list]
+print(res)
 # Output: [(9, 729), (5, 125), (6, 216)]
 # ```
 
 # Q94. Write a Python program to get all combinations of 2 tuples.
 # ```
-# Input : test_tuple1 = (7, 2), test_tuple2 = (7, 8)
+test_tuple1 = (7, 2) 
+test_tuple2 = (7, 8)
+res=[(a,b) for a in test_tuple1 for b in test_tuple2]
+res=res+[(a,b)for a in test_tuple2 for b in test_tuple1]
+print("Combination of tuples is :",str(res))
+
 # Output : [(7, 7), (7, 8), (2, 7), (2, 8), (7, 7), (7, 2), (8, 7), (8, 2)]
 # ```
 
 # Q95. Write a Python program to sort a list of tuples by second item.
 # ```
 # Input : [('for', 24), ('Geeks', 8), ('Geeks', 30)] 
+def Sort_Tuple(tup):
+     
+    lst = len(tup)
+    for i in range(0, lst):
+         
+        for j in range(0, lst-i-1):
+            if (tup[j][1] > tup[j + 1][1]):
+                temp = tup[j]
+                tup[j]= tup[j + 1]
+                tup[j + 1]= temp
+    return tup
+ 
+tup =[('452', 10), ('256', 5), ('100', 20), ('135', 15)]
+       
+print(Sort_Tuple(tup))
 # Output : [('Geeks', 8), ('for', 24), ('Geeks', 30)]
 # ```
 
 # Q96. Write a python program to print below pattern.
-# ```
+# n=int(input("Enter the value for start *: "))
+# for i in range(0,n):
+#     for j in range(0,i+1):
+#         print("*",end="")
+#     print("\r")
+# # ```
 # * 
 # * * 
 # * * * 
@@ -413,6 +490,10 @@ print(string_check(string_length,my_string))
 # * * * * * 
 # ```
 # Q97. Write a python program to print below pattern.
+# n=5;i=0
+# while(i<=n):
+#   print(" " * (n - i) +"*" * i)
+#   i+=1
 # ```
 #     *
 #    **
@@ -422,7 +503,12 @@ print(string_check(string_length,my_string))
 # ```
 
 # Q98. Write a python program to print below pattern.
-# ```
+# n=9
+# i=0
+# while i<n:
+#   print(' '*(n-i-1) + '* '* (i+1))   
+#   i+=1
+
 #     * 
 #    * * 
 #   * * * 
@@ -432,6 +518,12 @@ print(string_check(string_length,my_string))
 
 # Q99. Write a python program to print below pattern.
 # ```
+# n=5
+# for i in range(1,n+1):
+#   for j in range(1,i+1):
+#      print(j,end=" " )
+#   print()    
+
 # 1 
 # 1 2 
 # 1 2 3 
@@ -440,6 +532,14 @@ print(string_check(string_length,my_string))
 # ```
 
 # Q100. Write a python program to print below pattern.
+num=65
+n=5
+for i in range(0,n):
+    for j in range(0,i+1):
+        ch=chr(num)
+        print(ch,end=" ")
+    num=num+1
+    print("\r")    
 # ```
 # A 
 # B B 
